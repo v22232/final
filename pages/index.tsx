9a15abc6b8bd36e814a700e132ba1a7e9ca84ec2
김부희 <v@gimbuhuiui-MacBookPro.local>
2021-04-23 15:47:48 +0900

    Add favicon, head Tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -295,7 +295,7 @@ export default function indexPage() {
                         <p>
                             <Scramble
                                 autoStart
-                                text='PORTPOLIO'
+                                text='PORTFOLIO'
                                 speed='slow'
                                 steps={[
                                     {
